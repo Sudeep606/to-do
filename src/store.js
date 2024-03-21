8ee65590fa149e37409035fd603a92adca393253
Sudeep S <141109804+Sudeep606@users.noreply.github.com>
2024-03-21 15:56:52 +0530

    Add files via upload

---
+++ src/store.js
@@ -0,0 +1,26 @@
+import { configureStore, createSlice } from '@reduxjs/toolkit';
+
+const todosSlice = createSlice({
+  name: 'todos',
+  initialState: [],
+  reducers: {
+    addTodo: (state, action) => {
+      state.push(action.payload);
+    },
+    toggleTodo: (state, action) => {
+      const todo = state.find(todo => todo.id === action.payload);
+      if (todo) {
+        todo.completed = !todo.completed;
+      }
+    },
+    deleteTodo: (state, action) => {
+      return state.filter(todo => todo.id !== action.payload);
+    },
+  },
+});
+
+export const { addTodo, toggleTodo, deleteTodo } = todosSlice.actions;
+
+export default configureStore({
+  reducer: todosSlice.reducer,
+});
